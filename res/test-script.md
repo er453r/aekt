89b7cbbb802b6786842951922d793e3a20b31562
Marcin Kotz <marcin.kotz@ives-system.com>
2019-03-20 15:59:28 +0100

    some test files

---
+++ res/test-script.md
@@ -0,0 +1,47 @@
+# Testadventure
+
+
+See [my website][].
+
+[my website]: http://foo.bar.baz
+
+## Characters
+
+- ### Derp King
+
+    He is fat.
+
+- ### Blorp
+
+    He is lolz
+
+## Story
+
+### Act 1
+
+#### The question
+
+_Two omnious figures approach each other_
+
+[Blorp] Yo, wassup?  
+[Derp King] Nothing. Watching a game, having a bud...  
+[Blorp] True, true.  
+[Derp King] Hey, man what is 2 + 2?  *He asked politely*  
+> [Blorp] 6 (lie)   
+> [Derp King] Pffff... I don't think so. Are you sure? *derps quietly*
+>> [Blorp] Yup. (lie)  
+>> [Derp King] Wrong again.  
+>> [Blorp] Impossibru! ([The question])  
+>  
+>> [Blorp] 3?  
+>> [Derp King] Damn, you are stupid. ([The question])  
+   
+> [Blorp] 4  
+> [Derp King] Correctomundo!  
+
+[Blorp] Bye!  
+[Derp King] See you later.
+
+_Fade to black._
+
+## The end
--- src/com/er453r/ae/Parser.kt
+++ src/com/er453r/ae/Parser.kt
@@ -1,11 +1,100 @@
 package com.er453r.ae
 
 import mu.KotlinLogging
+import org.intellij.markdown.IElementType
+import org.intellij.markdown.MarkdownElementTypes
+import org.intellij.markdown.MarkdownTokenTypes
+import org.intellij.markdown.ast.ASTNode
+import org.intellij.markdown.ast.CompositeASTNode
+import org.intellij.markdown.ast.getTextInNode
+import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
+import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.parser.MarkdownParser
 
 class Parser {
     private val log = KotlinLogging.logger {}
 
     init {
-        log.info { "Parser start!" }
+        val src = getResourceAsText("/test-script.md")
+        val flavour = CommonMarkFlavourDescriptor()
+        val root = MarkdownParser(flavour).buildMarkdownTreeFromString(src)
+
+        //visitNode(root, src)
+
+        walk(root) { node, depth ->
+            val text = node.getTextInNode(src).take(10)
+
+            println("-".repeat(depth) + " ${node.type} - $text")
+
+            true
+        }
+
+        walk(root) { node, depth ->
+            when (node.type) {
+                MarkdownElementTypes.ATX_1 -> {
+                    println("Processing TITLE...")
+
+                    val title = node.findFirst(MarkdownTokenTypes.ATX_CONTENT)?.getTextInNode(src)
+
+                    println("Title is $title")
+                }
+                MarkdownElementTypes.ATX_2 -> {
+                    println("Processing SECTION...")
+
+                    val text = node.findFirst(MarkdownTokenTypes.ATX_CONTENT)?.getTextInNode(src)
+
+                    println("processing $text")
+
+                    walk(node) { node, depth ->
+                        when (node.type) {
+                            MarkdownElementTypes.ATX_3 -> {
+                                println("Processing SUB-SECTION...")
+
+                                val subsction = node.findFirst(MarkdownTokenTypes.ATX_CONTENT)?.getTextInNode(src)
+
+                                println("SUB-SECTION is $subsction")
+                            }
+                        }
+
+                        true
+                    }
+
+                    return@walk false
+                }
+                else -> {
+
+                }
+            }
+
+            true
+        }
+
+        val html = HtmlGenerator(src, root, flavour).generateHtml()
+
+        println("have html $html")
     }
+}
+
+fun getResourceAsText(path: String): String {
+    return object {}.javaClass.getResource(path).readText()
+}
+
+fun walk(node: ASTNode, depth: Int = 1, action: (ASTNode, Int) -> Boolean) {
+    val walkChildren = action(node, depth)
+
+    if (walkChildren && node is CompositeASTNode)
+        node.children.forEach {
+            walk(it, depth + 1, action)
+        }
+}
+
+fun ASTNode.findFirst(targetType: IElementType): ASTNode? {
+    if (this.type.name == targetType.name)
+        return this
+    else if (this is CompositeASTNode)
+        children.forEach { child ->
+            child.findFirst(targetType)?.let { return it }
+        }
+
+    return null
 }
