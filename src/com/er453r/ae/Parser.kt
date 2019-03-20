a1c03a91345c3896f7c3c683a5497715f0b9c5e5
Marcin Kotz <marcin.kotz@ives-system.com>
2019-03-20 15:46:39 +0100

    initial commit

---
+++ src/com/er453r/ae/Parser.kt
@@ -0,0 +1,11 @@
+package com.er453r.ae
+
+import mu.KotlinLogging
+
+class Parser {
+    private val log = KotlinLogging.logger {}
+
+    init {
+        log.info { "Parser start!" }
+    }
+}
---
+++ test/com/er453r/ae/ParserTests.kt
@@ -0,0 +1,15 @@
+package com.er453r.ae
+
+import mu.KotlinLogging
+import org.junit.Test
+
+class ParserTests {
+    private val log = KotlinLogging.logger {}
+
+    @Test
+    fun parse() {
+        log.info { "Parser test start!" }
+
+        Parser()
+    }
+}
